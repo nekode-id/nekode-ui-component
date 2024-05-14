8057f1e9f21de70423bd24eb3458e3440dccd0ac
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-05-14 18:21:02 +0700

    fix background color

--- src/components/ui/InvertedCursor/example.tsx
+++ src/components/ui/InvertedCursor/example.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import InvertedCursor from "./InvertedCursor";
 
 const InvertedCursorExample : React.FC = () => {
+
   return (
     <>  
-      <main className="grid place-items-center min-h-svh">
-         <h1 className='capitalize font-bold text-xl md:font-4xl'>Try to hover this text</h1>
+      <main className="grid place-items-center min-h-svh w-full bg-black">
+         <h1 className='capitalize font-bold text-xl md:font-4xl text-white'>Try to hover this text</h1>
       </main>
       <InvertedCursor />
     </>
